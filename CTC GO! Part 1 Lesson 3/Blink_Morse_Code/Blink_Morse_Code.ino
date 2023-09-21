07c0b10da36db931a6d8e2a6457f5090d47174c2
jhkeller171 <119902101+jhkeller171@users.noreply.github.com>
2023-09-21 13:30:05 -0400

    Added CTC GO! Part Lesson 3 and first blinks

---
+++ CTC GO! Part 1 Lesson 3/Blink_Morse_Code/Blink_Morse_Code.ino
@@ -0,0 +1,67 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+int LED13 = 13;
+int LED12 = 12;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED13, OUTPUT);
+  pinMode(LED12, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int count = 3;
+  while (count > 0){
+    digitalWrite(LED13, HIGH);
+    digitalWrite(LED12, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);                      // wait for a second
+    digitalWrite(LED13, LOW);
+    digitalWrite(LED12, LOW);   // turn the LED off by making the voltage LOW
+    delay(100);  
+    count = count-1;   
+  }              
+  count = 3;
+  while (count>0){
+    digitalWrite(LED13, HIGH);
+    digitalWrite(LED12, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(400);                      // wait for a second
+    digitalWrite(LED13, LOW);
+    digitalWrite(LED12, LOW);   // turn the LED off by making the voltage LOW
+    delay(400);  
+    count = count-1;
+  }
+  count = 3;
+  while (count>0){
+    digitalWrite(LED13, HIGH);
+    digitalWrite(LED12, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);                      // wait for a second
+    digitalWrite(LED13, LOW);
+    digitalWrite(LED12, LOW);   // turn the LED off by making the voltage LOW
+    delay(100);  
+    count = count-1;
+  }
+  delay(1000);
+}
