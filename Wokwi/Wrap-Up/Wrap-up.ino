c332f840a23c0ce2edede05667d1fd2c31594117
jhkeller171 <119902101+jhkeller171@users.noreply.github.com>
2023-10-13 08:20:13 -0400

    Update Wrap-up.ino

--- Wokwi/Wrap-Up/Wrap-up.ino
+++ Wokwi/Wrap-Up/Wrap-up.ino
@@ -1,7 +1,5 @@
-int sliderPin1 = 2;
-int sliderPin2 = 3;
-int sliderPin3 = 4;
-int sliderPin4 = 5;
+int sliderPin1 = 3;
+int sliderPin2 = 2;
 int ledPin1 = 9;
 int ledPin2 = 10;
 int slider1State = 0;
@@ -10,14 +8,12 @@ int slider2State = 0;
 void setup() {
   pinMode(sliderPin1, INPUT_PULLUP);
   pinMode(sliderPin2, INPUT_PULLUP);
-  pinMode(sliderPin3, INPUT_PULLUP);
-  pinMode(sliderPin4, INPUT_PULLUP);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
-  slider1State = digitalRead(sliderPin4);
+  slider1State = digitalRead(sliderPin1);
   slider2State = digitalRead(sliderPin2);
 
   if (slider1State == 1 && slider2State == 1){
