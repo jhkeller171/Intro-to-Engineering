4da3510f705a5b02a0e146581ffda2991c32c86b
jhkeller171 <119902101+jhkeller171@users.noreply.github.com>
2023-10-19 10:29:36 -0400

    Update Whack-a-mole.ino

--- Whack-a-mole/Whack-a-mole/Whack-a-mole.ino
+++ Whack-a-mole/Whack-a-mole/Whack-a-mole.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+int loseLED = 13;
+int winLED = 12;
+int yellowLED_1 = 11; 
+int yellowLED_2 = 10;
+int yellowLED_3 = 9;
 
+void setup() {
+  pinMode(loseLED, OUTPUT);
+  pinMode(winLED, OUTPUT);
+  pinMode(yellowLED_1, OUTPUT);
+  pinMode(yellowLED_2, OUTPUT);
+  pinMode(yellowLED_3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(loseLED, HIGH);
+  digitalWrite(winLED, HIGH); 
+  digitalWrite(yellowLED_1, HIGH);
+  digitalWrite(yellowLED_2, HIGH);
+  digitalWrite(yellowLED_3, HIGH);
+  delay(1000);
+  digitalWrite(loseLED, LOW);
+  digitalWrite(winLED, LOW);
+  digitalWrite(yellowLED_1, LOW);
+  digitalWrite(yellowLED_2, LOW);
+  digitalWrite(yellowLED_3, LOW);
+  delay(1000);
 }
