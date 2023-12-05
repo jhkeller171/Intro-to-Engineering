e919ad13180efa188f16aa066524df00ce313dc3
James Keller <119902101+jhkeller171@users.noreply.github.com>
2023-12-05 09:04:23 -0500

    Create challenge.ino

---
+++ Serial and Analog IO/AnalogInputs/challenge.ino
@@ -0,0 +1,44 @@
+int pot = A1; //Potentiometer
+int sense = A0; //Ultrasonic Sensor
+int trans = A2; //Phototransistor
+
+int potLED = 12; //Potentiometer LED
+int senseLED = 9; //Ultrasonic Senor LED
+int transLED = 13; //Phototransistor LED
+
+void setup() {
+  pinMode(potLED, OUTPUT);
+  pinMode(senseLED, OUTPUT);
+  pinMode(transLED, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  potLoop();
+  senseLoop();
+  transLoop();
+}
+
+void senseLoop(){ //Defines Ultra Loop
+  digitalWrite(senseLED, HIGH);
+  delay(analogRead(sense));
+  Serial.println("Ultraosnic Sensor: " + analogRead(sense));
+  digitalWrite(senseLED, LOW);
+  delay(analogRead(sense));
+}
+
+void transLoop(){ //Defines Phototransistor Loop
+  digitalWrite(transLED, HIGH);
+  delay(analogRead(trans));
+  digitalWrite(transLED, LOW);
+  delay(analogRead(sense));
+}
+
+void potLoop(){ //Defines Potentiamoetter Loop
+  digitalWrite(potLED, HIGH);
+  delay(analogRead(pot));
+  digitalWrite(potLED, LOW);
+  delay(analogRead(sense));
+}
+
